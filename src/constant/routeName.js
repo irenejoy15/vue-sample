dd5b3c3fb21c705af7f19a28d6ff58ce181d5e55
Cefrian Yves B. Trinchera <irenejoy15@gmail.com>
2026-06-03 13:06:11 +0800

    Routes and Constant

---
+++ src/constant/routeName.js
@@ -0,0 +1,11 @@
+export const APP_ROUTE_NAMES = Object.freeze({
+  HOME: 'home',
+  CONTACT_US: 'contact-us',
+  SIGN_IN: 'sign-in',
+  SIGN_UP: 'sign-up',
+  NOT_FOUND: 'not-found',
+  ACCESS_DENIED: 'access-denied',
+  PRODUCT_LIST: 'product-list',
+  PRODUCT_CREATE: 'product-create',
+  PRODUCT_UPDATE: 'product-update',
+})
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/home/Home.vue'
+import ContactUs from '@/views/home/ContactUs.vue'
+import SignIn from '@/views/auth/SignIn.vue'
+import SignUp from '@/views/auth/SignUp.vue'
+import NotFound from '@/views/auth/NotFound.vue'
+import AccessDenied from '@/views/auth/AccessDenied.vue'
+import ProductList from '@/views/product/ProductList.vue'
+import ProductUpsert from '@/views/product/ProductUpsert.vue'
+import { APP_ROUTE_NAMES } from '@/constant/routeName'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: APP_ROUTE_NAMES.HOME,
+      component: Home,
+    },
+    {
+      path: '/contact-us',
+      name: APP_ROUTE_NAMES.CONTACT_US,
+      component: ContactUs,
+    },
+    {
+      path: '/sign-in',
+      name: APP_ROUTE_NAMES.SIGN_IN,
+      component: SignIn,
+    },
+    {
+      path: '/sign-up',
+      name: APP_ROUTE_NAMES.SIGN_UP,
+      component: SignUp,
+    },
+    {
+      path: '/not-found',
+      name: APP_ROUTE_NAMES.NOT_FOUND,
+      component: NotFound,
+    },
+    {
+      path: '/access-denied',
+      name: APP_ROUTE_NAMES.ACCESS_DENIED,
+      component: AccessDenied,
+    },
+    {
+      path: '/product-list',
+      name: APP_ROUTE_NAMES.PRODUCT_LIST,
+      component: ProductList,
+    },
+    {
+      path: '/product-create',
+      name: APP_ROUTE_NAMES.PRODUCT_CREATE,
+      component: ProductUpsert,
+    },
+    {
+      path: '/product-update/:id',
+      name: APP_ROUTE_NAMES.PRODUCT_UPDATE,
+      component: ProductUpsert,
+    },
+  ],
 })
 
 export default router
